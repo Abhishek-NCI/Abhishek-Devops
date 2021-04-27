e0cfb74f561cca175c2ab2f98969e9070be04368
unknown <x19206925@student.ncirl.ie>
2021-04-27 20:01:44 +0100

    change in index file

--- index.php
+++ index.php
@@ -13,19 +13,19 @@
   <!-- The slideshow -->
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img src="images/image1.jpg" alt="Home Workout" width="1100" height="500">
+      <img src="Images/image1.jpg" alt="Home Workout" width="1100" height="500">
       <div class="carousel-caption">
       	<h2> BUY YOUR DREAM CAR WITH US </h2>
     </div>
     </div>
     <div class="carousel-item">
-      <img src="images/image3.jpg" alt="Yoga" width="1100" height="500">
+      <img src="Images/image3.jpg" alt="Yoga" width="1100" height="500">
       <div class="carousel-caption">
       	<h2> SELL YOUR USED CARS </h2>
     </div>
     </div>
     <div class="carousel-item">
-      <img src="images/image4.jpg" alt="Personalized" width="1100" height="500">
+      <img src="Images/image4.jpg" alt="Personalized" width="1100" height="500">
       <div class="carousel-caption">
       	<h2> BEST MARKET PRICES & DISCOUNTS ON CARS </h2>
       </div>
@@ -48,7 +48,7 @@
  	<div class="container-fluid">
  		<div class="row">
  		<div class="col-lg-6 col-md-6 col-12">
- 			<img src="images/vintage1.jpg" class="image-fluid aboutimg">	
+ 			<img src="Images/vintage1.jpg" class="image-fluid aboutimg">	
  		</div>	
  	<div class="col-lg-6 col-md-6 col-12">
  		<h2 class="display-4"> Why Choose Us ? </h2>
