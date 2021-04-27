1714a3f2371bdf71d7ab093e63e54b91b38d34cf
unknown <x19206925@student.ncirl.ie>
2021-04-27 19:55:07 +0100

    attaching rds to database

--- include/dbh.inc.php
+++ include/dbh.inc.php
@@ -1,8 +1,8 @@
 <?php 
 
-$serverName = "localhost";
-$dbusername = "root";
-$dbpassword = "";
+$serverName = "devopsdatabase01.ck8tyw9waixp.us-east-1.rds.amazonaws.com";
+$dbusername = "admin";
+$dbpassword = "admin1234";
 $dbname = "phpproject01";
 
 $conn = mysqli_connect($serverName, $dbusername, $dbpassword, $dbname);
